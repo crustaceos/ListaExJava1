0ea74eff965f1043fca3e685e1b2b0a6d444e7eb
Cauê Gonçalves <cauegon12@gmail.com>
2024-03-26 23:05:39 -0300

    programa

---
+++ programa.java
@@ -0,0 +1,27 @@
+public class programa {
+    public static void main (String[] args){
+    
+        //exercicio1.executar();
+        //exercicio2.executar();
+        //exercicio3.executar();
+        //exercicio4.executar();
+        //exercicio5.executar();
+        //exercicio6.executar();
+        //exercicio7.executar();
+        //exercicio8.executar();
+        //exercicio9.executar();
+        //exercicio10.executar();
+        //exercicio11.executar();
+        //exercicio12.executar();
+        //exercicio13.executar();
+        //exercicio14.executar();
+        //exercicio15.executar();
+        //exercicio16.executar();
+        //exercicio17.executar();
+        //exercicio18.executar();
+        //exercicio19.executar();
+        exercicio20.executar();
+
+
+    }
+}
